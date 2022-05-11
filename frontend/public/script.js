f75896bd7f4cc7169ca581d1128f3752b9d7aefa
gremgergo <gremgergo@gmail.com>
2022-05-11 12:00:27 +0200

    05.11 events

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,30 +1,74 @@
-const menuButtonComponent = function() {
+const menuButtonComponent = function() { // ha egy fuggvenyt ugy adunk at hogy ott a zarojel akkor a fuggveny erteket adjuk meg nem a fuggveny ezert nem call back fuggveny 
     return `
         <button id="menu-bt"></button>
     `
 }
 
+const beerNavComponent = function(inner) {
+    return `
+        <nav>${inner}</nav>
+    `
+}
+
+const beerAnchorComponent = function(title, id) {
+    return`
+        <a href="#${id}">${title}</a> 
+    ` // ha oldalon belul szeretnenk navigalni # kell href="#.."
+}
+
+const beerSectionComponent = function(title, sub, text, id) { 
+    return `
+        <section id="${id}">
+            <h1 class="beerName">${title}</h1>
+            <h2 class="beerCompany">${sub}</h2>
+            <h3 class="beerType">${text}</h3>
+        </section>
+    `
+}
+
+const menuButtonClickEvent = function (event) {
+    event.currentTarget.closest("#root").classList.toggle("menu-opened");
+
+}
+
 
 const loadEvent = function () {
+    /* console.log(typeof beerSectionComponent);
+    console.log(typeof beerSectionComponent());
+
+    (function () {
+        console.log("blalbabla");
+    })() */
+
     const rootElement = document.getElementById("root") // valtozot nem irunk le const, var nelkul 
 
-    console.log(rootElement.parentElement); // root element parentje a body
+    // console.log(rootElement.parentElement); // root element parentje a body
 
-    rootElement.insertAdjacentHTML("beforeend", menuButtonComponent())
+    rootElement.insertAdjacentHTML("beforeend", menuButtonComponent());
 
-    const menuButtonElement = document.getElementById("menu-bt") // ezzel jeloljuk ki a menu button id-t
+    const menuButtonElement = document.getElementById("menu-bt"); // ezzel jeloljuk ki a menu button id-t
 
-    menuButtonElement.addEventListener("click", function (event) { // a function() a call back function 
+    menuButtonElement.addEventListener("click", menuButtonClickEvent);
 
-        //event.currentTarget.classList.toggle("clicked") // toggle egy metodus ha nincs rajta rarakja ha rajta van leveszi
-        // console.log(rootElement);
-        // console.log(event.currentTarget.parentElement);
-        // console.log(event.currentTarget.closest("#root"));
-        event.currentTarget.closest("#root").classList.toggle("menu-opened");
-    })
+
+    let beerSections = ""
+
+    for (const beer of beers.cards) { // iterator barmi lehet 
+        beerSections += beerSectionComponent(beer.title, beer.sub, beer.text);
+    }
+    console.log(beerSections);
+    rootElement.insertAdjacentHTML("beforeend", beerSections);
+
+    let beerAnchors = ""
+
+    for (const beer of beers.cards) {
+        beerAnchors += beerAnchorComponent(beer.title)
+    }
+
+    rootElement.insertAdjacentHTML("beforeend", beerNavComponent(beerAnchors)) 
 }
 
 window.addEventListener("load", loadEvent) 
 
-
+// ha fuggveny utan van zarojel az egy lefuttando fuggveny ha nincs akkor egy callback function 
 
